1497de3899087a43541af64ad99f791b1057d361
Mario <mags9415@gmail.com>
2022-01-17 20:25:14 -0600

    Se acomodaron las peticiones en la base de datos ya trae las ofertas que son

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,37 +1,38 @@
-import { getConnection } from '../database/connection';
-import nodemailer from 'nodemailer';
-import { config } from 'dotenv';
+import { getConnection } from "../database/connection";
+import nodemailer from "nodemailer";
+import { config } from "dotenv";
 config();
 export const getOfertas = async (req, res) => {
         try {
                 const pool = await getConnection();
                 const result = await pool.request().query(`
-                SELECT
-                /*En esta parte pondremos las columnas de la tabla ofertasD*/
-                        oferta.ID,
-                        oferta.Articulo,
-                        oferta.Unidad as UnidadCompra,
-                        CostoBase,
-                        /*En esta parte pondremos las columnas de la tabla Oferta donde tenemos el estatus de VIGENCIA*/
-                        --vigencia.Articulo,
-                        vigencia.Referencia,
-                        /*vigencia.Unidad,*/
-                        --vigencia.Empresa,
-                        vigencia.MovID, --este es para saber quien fue el que dio de alta la oferta 
-                        vigencia.Estatus,
-                        /*En esta parte pondremos las columnas de la tabla Articulos de esta traemos la descripcion y la linea a la que pertence*/
-                        articulo.Descripcion1,
-                        articulo.Linea,
-                        articulo.Unidad as UnidadVenta
-                FROM [${process.env.TABLA}].[${process.env.DBO}].OfertaD  As oferta
-                INNER JOIN [${process.env.TABLA}].[${process.env.DBO}].Oferta as vigencia
-                ON(oferta.ID = vigencia.ID)
-                INNER JOIN [${process.env.TABLA}].[${process.env.DBO}].Art as articulo
-                ON articulo.Articulo = oferta.Articulo
-                WHERE  vigencia.Estatus ='VIGENTE' and (vigencia.Referencia ='mayoreo super' or vigencia.Referencia ='pueblita'
-                or vigencia.Referencia= 'mostrador y super' or vigencia.Referencia='PUEBLITA OFERTAS')
-                -- falta expecificar  y estandarizar las referencias and (vigencia.Referencia ='pueblita' OR vigencia.Referencia ='mostrador')
-                order by oferta.ID asc`);
+               
+ SELECT 
+/*En esta parte pondremos las columnas de la tabla ofertasD*/
+        oferta.ID,
+        oferta.Articulo,
+        oferta.Unidad as UnidadCompra,
+        CostoBase,
+        /*En esta parte pondremos las columnas de la tabla Oferta donde tenemos el estatus de VIGENCIA*/
+        --vigencia.Articulo,
+        vigencia.Referencia,
+        /*vigencia.Unidad,*/
+        --vigencia.Empresa,
+        vigencia.MovID, --este es para saber quien fue el que dio de alta la oferta 
+        vigencia.Estatus,
+        /*En esta parte pondremos las columnas de la tabla Articulos de esta traemos la descripcion y la linea a la que pertence*/
+        articulo.Descripcion1,
+        articulo.Linea,
+        articulo.Unidad as UnidadVenta
+FROM [ABATZ].[dbo].OfertaD  As oferta
+INNER JOIN [ABATZ].[dbo].Oferta as vigencia
+ON(oferta.ID = vigencia.ID)
+INNER JOIN [ABATZ].[dbo].Art as articulo
+ON articulo.Articulo = oferta.Articulo
+WHERE  vigencia.Estatus ='VIGENTE' and (vigencia.Referencia ='OFERTA PUEBLITA' or vigencia.Referencia ='OFERTA SUPER Y MOSTRADOR'
+or vigencia.Referencia= 'OFERTA ALMACEN y super' or vigencia.Referencia='PUEBLITA OFERTAS')
+-- falta expecificar  y estandarizar las referencias and (vigencia.Referencia ='pueblita' OR vigencia.Referencia ='mostrador')
+order by oferta.ID asc`);
                 const a = result.recordset;
                 // no se ocupa para que no se alente el api console.log(result.recordset);
                 res.json(a);
@@ -77,16 +78,16 @@ export const sendEmail = async (req, res) => {
 
                 // send mail with defined transport object
                 let info = await transporter.sendMail({
-                        from: 'Pagina <contacto@abattz.com>', // El correo que lo envia
-                        to: 'contacto@abattz.com', // el correo destinatario
-                        subject: 'Prueba de mensaje', //Asunto
-                        text: 'enviado desde la aplicacion', // mensaje
+                        from: "Pagina <contacto@abattz.com>", // El correo que lo envia
+                        to: "contacto@abattz.com", // el correo destinatario
+                        subject: "Prueba de mensaje", //Asunto
+                        text: "enviado desde la aplicacion", // mensaje
                         html: contentHTML, // Este es el formulario que creamos arriba y acomodamos deacuerdo a nuestros datos enviados desde la pagina
                 });
 
-                console.log('enviado');
+                console.log("enviado");
 
-                res.send('enviado');
+                res.send("enviado");
         } catch (error) {
                 console.error(`${error}`);
         }
